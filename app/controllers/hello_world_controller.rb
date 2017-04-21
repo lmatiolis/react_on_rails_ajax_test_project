c272cc96c253c74ab4d62b4d074d53a031f4dc07
lmatiolis <matioli.leandro@gmail.com>
2017-04-21 16:26:54 -0300

    Show react on rails ajax call reload issue.
    Related to shakacode/react_on_rails#549

--- app/controllers/hello_world_controller.rb
+++ app/controllers/hello_world_controller.rb
@@ -1,5 +1,10 @@
 class HelloWorldController < ApplicationController
   def index
     @hello_world_props = { name: "Stranger" }
+    @hello_world2_props = { name: "Stranger2" }
+  end
+
+  def reload
+    @hello_world_props = { name: "Stranger Reloaded" }
   end
 end
